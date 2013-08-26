1b80f9d1756b7f3c10df8ee7939ae623851dc658
Shana Weitzen <shanaweitzen@gmail.com>
2013-08-26 11:15:44 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Koala::Application.config.secret_key_base = '4a29231815ab33107dc1282d398e8ad08647e940edb36549fc2552a1e9f05c066100c90a38ce02862301aeeb6122195e9a04d920c66e92dfeb1aa99b6547513a'
