15f6e79149c8e6dbd077a8a20d31e9bf16dd2f5d
Shana Weitzen <shanaweitzen@gmail.com>
2013-09-09 15:02:52 -0700

    clean js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,29 +1,13 @@
-///pusher ///
-
 $(document).ready(function(){
-
-
 	holder = $("#holder");
-
 	if( holder.length > 0 ){
 
-		 // Enable pusher logging - don't include this in production
-		 // Pusher.log = function(message) {
-	  //     if (window.console && window.console.log) {
-	  //       window.console.log(message);
-	  //     }
-	  //   };
-	    
-	    var pusher = new Pusher(pusher_key); //my pusher key
+	    var pusher = new Pusher(pusher_key); 
 	    var channel = pusher.subscribe('messages'); 
-	    // the channel I'm subscribing to, it could be anything
+	    
 
 	    channel.bind('new_message', function(data) { //reading new message from the channel the specific channel
 	    	$("#messageWindow li").last().append('<li>' + data +' </li>');
-
-				//inserts the new message as the last li
-	  		
-	  		// alert('An event was triggered with message: ' + data.text);
 	    });
 	 }	
 
@@ -31,29 +15,17 @@ $(document).ready(function(){
 	$.ajax('/chats.json', {
 		success: function(data){
 			var template = $('#template').html();
-			// console.log(data);
 			var renderHTML = Mustache.to_html(template, data);
 			$('#holder').html(renderHTML);
-
 			$(".message-btn").on( "click", message_submit);
-
 		}
 	});
-
-
-
 
 });
 
 function message_submit(){
-	console.log('SEND MESSAGE');
-
 	user_id = $(this).data("user_id");
-	console.log("USER_ID " + user_id);
-
 	text = $('#message_field').val();
-	console.log(message_field);
-
 	JSON_data = JSON.stringify( {"text":text, "user_id": user_id} );
 
     path = "/messages";
@@ -62,12 +34,8 @@ function message_submit(){
     type: "POST",
     url: path,
     data: JSON_data,
-    contentType: "application/json",
+    contentType: "application/json"
 
-    complete : function(r) {
-    	console.log("POSTED COMMAND");
-      // window.location.replace(path)
-    }
   });
 	return false;
 }
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,10 @@
 class MessagesController < ApplicationController
 
 	def create
-		puts "\n\n HERE \n\n"
 		@message = Message.new(message_params)
 		if @message.save!
 			# i need a response back in JSON so I know it saved in the DB
 			Pusher.trigger('messages', "new_message", @message.text_with_user_name) #trigger message channel channel, evnt, data
-			puts "\n\n here \n\n"
 			render :json => "", :code => :ok
 		end
 	end
