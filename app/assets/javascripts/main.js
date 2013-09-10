2e1d76b1ac2b754cc2e31bdd3f36b962db496430
Shana Weitzen <shanaweitzen@gmail.com>
2013-09-09 17:10:50 -0700

    fix message_field, js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -17,28 +17,30 @@ $(document).ready(function(){
 			var template = $('#template').html();
 			var renderHTML = Mustache.to_html(template, data);
 			$('#holder').html(renderHTML);
-			$(".message-btn").on( "click", message_submit);
+
+			$(".message-btn").click(function(event){
+				message_submit();
+				event.preventDefault();
+			});
+
+			$("#message_field").keypress(function(event) {
+			  	if ( event.which == 13 ) {
+					message_submit();
+					event.preventDefault();
+				}
+			});
+
 		}
 	});
 
 });
 
 function message_submit(){
-	user_id = $(this).data("user_id");
-	text = $('#message_field').val();
-	JSON_data = JSON.stringify( {"text":text, "user_id": user_id} );
-
-    path = "/messages";
-  
-  $.ajax({
-    type: "POST",
-    url: path,
-    data: JSON_data,
-    contentType: "application/json"
-
-  });
+	$('.message_field form').submit();
+	$('#message_field').val("");
 	return false;
 }
+
 
 	    	
 	  
